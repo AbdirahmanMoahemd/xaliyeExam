52b3b0deb92781a750b7e859beeb6f334647d8e9
unknown <kaah6978@gmail.com>
2023-03-12 14:08:09 +0300

    initial commit

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -21,20 +21,20 @@ import { admin, protect } from "../middlewares/authMiddleware.js";
 
 // router.route("/").post(registerUser).get(protect, admin, getUsers);
 // router.get("/count", getUsersCount);
-// router.post("/register", registerUser2);
-// router.post("/login", authUser);
-// router.post("/app/login", authUser2);
+router.post("/register", registerUser2);
+router.post("/login", authUser);
+router.post("/app/login", authUser2);
 // router
 //   .route("/profile")
 //   .get(protect, getUserProfile)
 //   .put(protect, updateProfile);
-// router.route('/profile/:id').post(getUserProfileById)
+router.route('/profile/:id').post(getUserProfileById)
 // router.route('/save-user-address').put(protect, saveUserAddress)
 // router.route('/update/forgot/email').post(forgottUserPassword)
 // router.route("/profile/password").put(protect, updateUserPassword);
-// router.route("/:id").get(protect, getUserById);
+router.route("/:id").get(protect, getUserById);
 //   .delete(protect, admin, deleteUser)
 
-//   .put(protect, admin, updateUser);
+  // .put(protect, admin, updateUser);
 
 export default router;
